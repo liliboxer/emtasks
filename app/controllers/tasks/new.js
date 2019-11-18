9018b1f798ff068c63e8807922a735d2bdb9dc52
Lili Boxer <ninaliboxer@gmail.com>
2019-11-18 14:15:30 -0800

    addTask functionality set up

--- app/controllers/tasks/new.js
+++ app/controllers/tasks/new.js
@@ -1,4 +1,13 @@
 import Controller from '@ember/controller';
 
 export default Controller.extend({
+  actions: {
+    addTask: function() {
+      let title = this.get('title');
+      let description = this.get('description');
+      let date = this.get('date');
+
+      // create new task
+    }
+  }
 });
